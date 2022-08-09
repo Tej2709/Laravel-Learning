1a4ba9def6cb2420405866580922d9c159daee46
Tej2709 <tejsoni0022@gmail.com>
2022-08-09 18:04:19 +0530

    Yajra DataTable Example with Package

---
+++ YajraExample/resources/views/employee.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <title>DataTable Example</title>
+</head>
+
+<body>
+    <section style="padding-top:60px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    {!! $dataTable->table() !!}
+                </div>
+            </div>
+        </div>
+    </section>
+    {!! $dataTable->scripts() !!}
+</body>
+
+</html>
